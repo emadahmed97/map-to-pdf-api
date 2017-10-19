5250d2f290520d26232d4b075694f9ab66aa29be
emadahmed97 <emad1997@gmail.com>
2017-10-19 02:57:40 -0400

    testing again44

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.all('/', function(req, res, next) {
 app.post('/', function(req, res){
   var jsonRequest = req.body;
   console.log(Object.keys(jsonRequest)[0]);
-  res.send(JSON.stringify(req.body));
+  res.send(req.body);
 
 });
 
