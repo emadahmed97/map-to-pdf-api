965230fed7d542907858f74aaf36f4a73381e268
emadahmed97 <emad1997@gmail.com>
2017-10-19 01:27:15 -0400

    updated brackets

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.post('/', function(req, res){
   var base64Image = new Buffer(url, 'binary').toString('base64')
   var result = 'data:image/jpeg;base64,' + base64Image
   res.json(result);
-}
+});
 
 var server = app.listen( process.env.PORT || 3000, function(){
   console.log('Server listening on port ' + process.env.PORT || 3000  );
